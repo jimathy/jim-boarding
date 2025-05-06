e6650a9ff5af01e0087c12200e4917b1f6224e37
Jim Shield <gothicjim@gmail.com>
2025-05-06 22:45:47 +0100

    Update en.lua

--- locales/en.lua
+++ locales/en.lua
@@ -1,6 +1,31 @@
 Loc = Loc or {}
 
 Loc["en"] = {
-    getoff = "Get Off Skateboard",
-    lockcam = "Unlock/Lock Cam",
+    info = {
+        getoff = "Get Off Skateboard",
+        getoffSurf = "Get Off Surfboard",
+        lockcam = "Lock Cam",
+        jump = "Jump",
+        putDown = "[Backspace] Put board down"
+    },
+    keyMaps = {
+        lockCam = "Skateboard: Unlock/Lock Cam",
+        flipCam = "Skateboard: Flip Cam",
+        getOff = "Skateboard: Get Off'",
+        forward = "Skateboard: Forward",
+        backward = "Skateboard: Backward",
+        left = "Skateboard: Left",
+        right = "Skateboard: Right",
+        jump = "Skateboard: Jump",
+    },
+    menus = {
+        holdup = "Hold Up",
+        header = "Show The Board Off",
+    },
+    targets = {
+        getOn = "Get on",
+        pickUp = "Pick up",
+        showOff = "Show Off",
+        hold = "Hold",
+    },
 }
