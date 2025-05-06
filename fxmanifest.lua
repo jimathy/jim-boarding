d8d7d3458ae5415bf39baade355e9cb61b59a1cf
Jim Shield <gothicjim@gmail.com>
2025-05-06 22:47:10 +0100

    Version Bump
    `2.0.1` - `2.1.0`

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,27 +1,34 @@
-name "jim-skateboard"
+name "jim-boarding"
 author "Jimathy"
-version "2.0.1"
-description "Electric Skateboard Script By Jimathy"
+version "2.1.0"
+description "Electric Skateboard/Surfboard Script"
 fx_version "cerulean"
 game "gta5"
 lua54 'yes'
 
+server_script '@oxmysql/lib/MySQL.lua'
+
 shared_scripts {
     'locales/*.lua',
+
     'config.lua',
+
     -- Required core scripts
     '@ox_lib/init.lua',
-    '@ox_core/imports/client.lua',
+    '@ox_core/lib/init.lua',
+
     '@es_extended/imports.lua',
+
     '@qbx_core/modules/playerdata.lua',
 
     --Jim Bridge
-    '@jim_bridge/exports.lua',
-    '@jim_bridge/functions.lua',
-    '@jim_bridge/wrapper.lua',
-    '@jim_bridge/crafting.lua',
-}
-client_script 'client.lua'
-server_script 'server.lua'
+    '@jim_bridge/starter.lua',
 
-dependancy 'jim_bridge'
+	'shared/*.lua'
+}
+
+client_scripts { 'client/*.lua' }
+
+server_script { 'server/*.lua' }
+
+dependency 'jim_bridge'
