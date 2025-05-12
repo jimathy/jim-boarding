a9767ef9ac675835cf67d83fe0b3e0b5f8d8a27c
Jim Shield <gothicjim@gmail.com>
2025-05-12 22:52:10 +0100

    place start functions behind onResourceStart()

--- server/usableItems.lua
+++ server/usableItems.lua
@@ -1,11 +1,13 @@
-for k, v in pairs(SkateboardItemModels) do
-    createUseableItem(k, function(source, item)
-        TriggerClientEvent(getScript()..":Skateboard:PickPlace", source, { name = k })
-    end)
-end
+onResourceStart(function()
+    for k, v in pairs(SkateboardItemModels) do
+        createUseableItem(k, function(source, item)
+            TriggerClientEvent(getScript()..":Skateboard:PickPlace", source, { name = k })
+        end)
+    end
 
-for k, v in pairs(SurfboardItemModels) do
-    createUseableItem(k, function(source, item)
-        TriggerClientEvent(getScript()..":SurfBoard:PickPlace", source, { name = k })
-    end)
-end
+    for k, v in pairs(SurfboardItemModels) do
+        createUseableItem(k, function(source, item)
+            TriggerClientEvent(getScript()..":SurfBoard:PickPlace", source, { name = k })
+        end)
+    end
+end, true)
