b12b0a0f335d9c6f046723157e4cb9570cbadf00
derTweaky <jan.kuechenmeister@gmail.com>
2025-07-31 07:33:44 +0200

    Create de.lua

---
+++ locales/de.lua
@@ -0,0 +1,31 @@
+Loc = Loc or {}
+
+Loc["de"] = {
+    info = {
+        getoff = "Vom Skateboard absteigen",
+        getoffSurf = "Vom Surfbrett absteigen",
+        lockcam = "Kamera sperren",
+        jump = "Springen",
+        putDown = "[Backspace] Board ablegen"
+    },
+    keyMaps = {
+        lockCam = "Skateboard: Kamera sperren/entsperren",
+        flipCam = "Skateboard: Kamera wechseln",
+        getOff = "Skateboard: Absteigen",
+        forward = "Skateboard: Vorwärts",
+        backward = "Skateboard: Rückwärts",
+        left = "Skateboard: Links",
+        right = "Skateboard: Rechts",
+        jump = "Skateboard: Springen",
+    },
+    menus = {
+        holdup = "Hochhalten",
+        header = "Board präsentieren",
+    },
+    targets = {
+        getOn = "Aufsteigen",
+        pickUp = "Aufheben",
+        showOff = "Vorzeigen",
+        hold = "Halten",
+    },
+}
